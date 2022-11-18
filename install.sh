907a74ad6f9bfd237992b4f7fe2aaae47599caa8
Kartik Deshmukh <63499572+MelloB1989@users.noreply.github.com>
2022-11-18 21:17:26 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
-echo 'Installing will start shortly...'
-echo 'Developed and managed by MelloB'
+echo -e '\nInstalling will start shortly...'
+echo -e '\nDeveloped and managed by MelloB'
+echo -e '\n NOTE: PRESS ENTER IF ASKED ANY PROMPT FOR DEFAULT INSTALLATION!'
 sleep 2s
-pkg update && pkg upgrade -y
+pkg update -y && pkg upgrade -y
 pkg install figlet wget nano git clang openssh ruby -y
 wget https://github.com/busyloop/lolcat/archive/master.zip
 unzip master.zip
@@ -10,14 +11,15 @@ gem install lolcat
 figlet MelloB | lolcat
 echo 'Finishing installation....' | lolcat
 sleep 2s
-cp bash.bashrc /data/data/com.termux/files/
+cp ~/termux_clang_install/bash.bashrc /data/data/com.termux/files/usr/etc/
 termux-reload-settings
-cp runc $PATH
-cp update $PATH
-cp termux.properties ~/.termux/
-cp font.ttf ~/.termux/
-cp color.properties ~/.termux/
+cp ~/termux_clang_install/runc $PATH
+cp ~/termux_clang_install/update $PATH
+cp ~/termux_clang_install/termux.properties ~/.termux/
+cp ~/termux_clang_install/font.ttf ~/.termux/
+cp ~/termux_clang_install/colors.properties ~/.termux/
 
 figlet Termux C | lolcat
 echo 'Installation is complete...Happy Hacking!'
+sleep 2s
 exit
